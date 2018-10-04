0852568142042f65cd9d3a270d39d6ddbaff8f2b
Marton Orova <marton.orova@gmail.com>
2018-10-04 21:35:18 +0200

    Create parameterizedTest for LRUPL, image of original test

--- src/test/java/logictest/LRUPLTest.java
+++ src/test/java/logictest/LRUPLTest.java
@@ -1,30 +1,45 @@
 package logictest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import logic.LRUPL;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class LRUPLTest {
-	LRUPL lpl = new LRUPL();
+	//private static LRUPL lrupl;
+
+	@BeforeAll
+    static void initAll() {
+	    //lrupl = new LRUPL();
+    }
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
-	@Test
-	void test() {
-		assertEquals('A', lpl.allocate(1));
-		assertEquals('B', lpl.allocate(2));
-		assertEquals('C', lpl.allocate(3));
-		assertEquals('D', lpl.allocate(4));
-		
-		assertEquals('*', lpl.allocate(5));
-//		assertEquals('A', lpl.allocate(6));
-//		assertEquals('*', lpl.allocate(7));
-		
+	@ParameterizedTest
+    @MethodSource("testAllocateProvider")
+	void testAllocate(char frameName, int counter, LRUPL lrupl) {
+	    assertEquals(frameName, lrupl.allocate(counter));
 	}
+
+	static Stream<Arguments> testAllocateProvider() {
+	    LRUPL lrupl = new LRUPL();
+	    return Stream.of(
+	            Arguments.of('A', 1, lrupl),
+                Arguments.of('B', 2, lrupl),
+                Arguments.of('C', 3, lrupl),
+                Arguments.of('D', 4, lrupl),
+                Arguments.of('*', 5, lrupl)
+                //Arguments.of('A', 6, lrupl),
+                //Arguments.of('*', 7, lrupl)
+        );
+    }
 
 }
